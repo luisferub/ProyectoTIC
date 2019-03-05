6fc925aa2968b8732a968489c8c63ecafef64f44
luisferub <luisferub@gmail.com>
2019-03-04 22:35:38 -0500

    version 0.1

---
+++ app/src/main/java/com/user/discoverfood/MainActivity.java
@@ -0,0 +1,35 @@
+package com.user.discoverfood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    void verMenu(View view){
+        Intent intencion = new Intent(this, MenuActivity.class);
+        startActivity(intencion);
+    }
+
+    void verRestaurantes(View view){
+        Intent intencion = new Intent(this, RestaurantesActivity.class);
+        startActivity(intencion);
+    }
+
+    void verLogin(View view){
+        Intent intencion = new Intent(this, LoginActivity.class);
+        startActivity(intencion);
+    }
+
+    void verContacto(View view){
+        Intent intencion = new Intent(this, ContactoActivity.class);
+        startActivity(intencion);
+    }
+}
