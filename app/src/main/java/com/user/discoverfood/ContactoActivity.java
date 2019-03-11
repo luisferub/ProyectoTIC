513a9a8fff4df4b177de119e74c78b4f8aaf137f
luisferub <luisferub@gmail.com>
2019-03-11 01:17:10 -0500

    version 0.3

--- app/src/main/java/com/user/discoverfood/ContactoActivity.java
+++ app/src/main/java/com/user/discoverfood/ContactoActivity.java
@@ -1,7 +1,9 @@
 package com.user.discoverfood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ContactoActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ContactoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacto);
+    }
+
+    void verMain(View view){
+        Intent intencion = new Intent(this, MainActivity.class);
+        startActivity(intencion);
     }
 }
