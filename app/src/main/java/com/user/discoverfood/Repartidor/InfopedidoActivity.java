439d91b0048368645bdd0ccaca5871526aeab37b
luisferub <luisferub@gmail.com>
2019-05-21 00:13:29 -0500

    version 0.91

--- app/src/main/java/com/user/discoverfood/Repartidor/InfopedidoActivity.java
+++ app/src/main/java/com/user/discoverfood/Repartidor/InfopedidoActivity.java
@@ -2,6 +2,8 @@ package com.user.discoverfood.Repartidor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.user.discoverfood.R;
@@ -9,6 +11,7 @@ import com.user.discoverfood.R;
 public class InfopedidoActivity extends AppCompatActivity {
 
     TextView nombre, precio, pedido, telefono;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +29,14 @@ public class InfopedidoActivity extends AppCompatActivity {
 
         telefono = (TextView) findViewById(R.id.telefono);
         telefono.setText("Telefono: " + RepartidorActivity.telefonoAux);
+
+        btn=(Button)findViewById(R.id.prevButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
